1e49edf6ca0d074ab0a910dadebca7b93ba21ef5
Boroczki Ferenc <ferenc.boroczki@gmail.com>
2013-07-24 20:50:35 +0000

    read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
+var fs = requre('fs');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');
+    var contentBuffer = fs.readFileSync("index.html");
+    var contentString = contentBuffer.toString();
+    response.send(contentString);
 });
 
 var port = process.env.PORT || 5000;
